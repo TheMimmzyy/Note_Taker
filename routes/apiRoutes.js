dc3fe8275d68d207982534e2a2398595029676e1
TheMimmzyy <cjc3481@gmail.com>
2021-07-18 21:15:45 -0600

    refactored

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,19 @@ const fs = require("fs");
 var path = require("path");
 
 module.exports = function (app) {
-  // Read the db.json file
   let objectsList = JSON.parse(fs.readFileSync("./db/db.json", "utf8", (err) => {
     if (err) throw err;
   }));
 
-  // API GET Requests
   app.get("/api/notes", function (req, res) {
     return res.json(objectsList)
   })
 
-  // API POST Requests
   app.post("/api/notes", function (req, res) {
-    let newNote = { title: req.body.title, text: req.body.text }; // New object built with 
-    newNote.id = objectsList.length.toString(); // convert to string and add to note
+    let newNote = { title: req.body.title, text: req.body.text };
+    newNote.id = objectsList.length.toString();
 
-    objectsList.push(newNote); // Add new object to objects list
+    objectsList.push(newNote);
 
     fs.writeFile("./db/db.json", JSON.stringify(objectsList), (err) => {
       if (err) throw err;
@@ -28,12 +25,10 @@ module.exports = function (app) {
 
   // API DELETE Requests  
   app.delete("/api/notes/:id", function (req, res) {
-    let idSelected = JSON.parse(req.params.id);   // Get the id selected by the user
-    // New array of objects by filtering out the object with the selected id
+    let idSelected = JSON.parse(req.params.id);
     objectsList = objectsList.filter((e) => {
       return e.id != idSelected;
     });
-    // Reassign object.id to array index of objectsList
     objectsList.forEach((val, index) => {
       val.id = index.toString();
     });
